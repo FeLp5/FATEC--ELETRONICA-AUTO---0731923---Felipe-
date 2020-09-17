b996c87a5c4a01e522cbbe37f8a8049de992e0e5
FeLp5 <69984907+FeLp5@users.noreply.github.com>
2020-09-17 20:02:52 -0300

    Add files via upload

---
+++ Seta.c
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ FileName:     Seta - FATEC SANTO ANDRÉ
+ Dependencies: Veja a seção de includes
+ Processor:    PIC18F4550
+ Compiler:     MPLAB X v3.51 + XC8 1.41
+ Company:      FATEC Santo Andre
+ Author:       Felipe Alves Leite Da Silva
+ Date:         17/09/2020
+ Software License Agreement: Somente para fins didáticos
+ *******************************************************************************
+ File Description: Este programa simula a seta do carro, fazendo o led piscar em loop em função de um botão.
+ Change History:
+ 1.0   17/09/2020  Versão inicial
+ 
+*******************************************************************************/
+
+
+//Includes do compilador
+
+#include <p18f4550.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//Includes do projeto
+
+#include "Config.h"
+
+//Define's
+
+
+
+#define setaE LATDbits.LATD7
+#define setaD LATDbits.LATD0
+#define B1 PORTBbits.RB0
+#define B2 PORTBbits.RB1
+
+
+void delay_ms(unsigned int tempo);
+void main(void) 
+{  
+    ADCON1 = 0x0F;
+    TRISB = 0b11111111;
+    TRISD = 0b00000000;
+    delay_ms(100);
+    while(1)
+    {
+        if(B1==0)
+        {
+            setaE = 1;
+            delay_ms(500);
+            setaE = 0;
+            delay_ms(500);
+        }else if(B2==0)
+        {
+            setaD = 1;
+            delay_ms(500);
+            setaD = 0;
+            delay_ms(100);
+        }else{
+            setaE = 0;
+            setaD = 0;
+            
+        }
+    }
+    
+    
+    return;
+}
+
+/******************************************************************************               
+* Funcao:   delay_ms(unsigned int tempo)
+* Entrada:  unsigned int tempo - recebe valores de 0 a 65536
+* Saida:    Nenhuma (void)
+* Descricao: Gera um atraso de tempo conforme o valor recebido de delaytime
+             O tempo interno é baseado no delay 1 ms. Assim, a cada loop temos o
+ *           decremento da variável delaytime, até que ele atinja zero. Exemplo:
+ *           se o delyatime = 500 a função terminará e retornará para o programa
+ *           principal após 500 ms.
+/******************************************************************************/
+void delay_ms(unsigned int tempo)
+{
+    while(--tempo)
+    {
+        __delay_ms(1);//gera um atraso de 1ms
+    }  
+}
